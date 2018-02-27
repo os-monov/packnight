a3431510f6baa85ba7619aed33ace731c239e3f7
Jermaine <Jjmunky@users.noreply.github.com>
2018-02-26 18:15:49 -0800

    Add files via upload
    Rooms prob works

--- proj2/byog/Core/Hallway.java
+++ proj2/byog/Core/Hallway.java
@@ -4,111 +4,37 @@ import byog.TileEngine.Tileset;
 import byog.TileEngine.TETile;
 
 public class Hallway {
-    static Integer[][] blocks_to_delete = new Integer[1000][2]; // 2D array of the x,y coordinates of blocks we want to replace with floor
-    static int remove_size = 0; // size of 2D array
+    int length;
+    int x_start;
+    int y_start;
+    Integer[][] blocks_to_delete = new Integer[1000][2];
+    int helper_index_array = 0;
 
 
     public Hallway(){
-    }
-
-    ///////////////////////////////////////////////////////////////////////////
-
-    public static void drawHorizontalHallwayRight(int x, int y, int len){
-        for (int i = x; i < (x + len); i += 1){
-
-            if (i == 79) {
-                break;
-            }
-
-            else{
-                MapGenerator.TETile_world[i][y + 1] = Tileset.WALL;
-                MapGenerator.TETile_world[i][y - 1] = Tileset.WALL;
-                MapGenerator.TETile_world[i][y] = Tileset.WALL;
-                blocks_to_delete[remove_size] = new Integer[]{i, y};
-                remove_size += 1;
-                }
-
-            }
+        Random r = new Random();
+        int length = RandomUtils.uniform(r, 10);
+        int x_start = RandomUtils.uniform(r, 79);
+        int y_start = RandomUtils.uniform(r, 30);
     }
 
 
-    ///////////////////////////////////////////////////////////////////////////
+    //firstHallway = Hallway();
 
-    public static void drawHorizontalHallwayLeft(int x, int y, int len){
-        for (int i = x; i > (x - len); i -= 1){
-            if (i == 0){
-                break;
-            }
-
-            else {
-                MapGenerator.TETile_world[i][y + 1] = Tileset.WALL;
-                MapGenerator.TETile_world[i][y - 1] = Tileset.WALL;
-                MapGenerator.TETile_world[i][y] = Tileset.WALL;
-                blocks_to_delete[remove_size] = new Integer[]{i, y};
-                remove_size += 1;
-
-            }
-
+    public void drawHorizontalHallway(){
+        for (int i = x_start; i < (x_start + length); i += 1){
+            MapGenerator.TETile_world[i][y_start + 1] = Tileset.WALL;
+            MapGenerator.TETile_world[i][y_start - 1] = Tileset.WALL;
+            MapGenerator.TETile_world[i][y_start] = Tileset.WALL;
+            blocks_to_delete[helper_index_array] = new Integer[]{i, y_start};
         }
     }
 
-    ///////////////////////////////////////////////////////////////////////////
-
-    public static void drawVerticalHallwayUp(int x, int y, int len){
-        for (int i = y; i < (y + len); i += 1){
-            if (i == 29){
-                break;
-            }
-
-            else {
-                MapGenerator.TETile_world[i][x + 1] = Tileset.WALL;
-                MapGenerator.TETile_world[i][x - 1] = Tileset.WALL;
-                MapGenerator.TETile_world[i][x] = Tileset.WALL;
-                blocks_to_delete[remove_size] = new Integer[]{i, x};
-                remove_size += 1;
-            }
+    public void drawVerticalHallway(){
+        for (int i = y_start; i < (y_start + length); i += 1){
+            MapGenerator.TETile_world[i][x_start + 1] = Tileset.WALL;
+            MapGenerator.TETile_world[i][x_start - 1] = Tileset.WALL;
+            MapGenerator.TETile_world[i][x_start] = Tileset.FLOWER;
         }
     }
-
-    ///////////////////////////////////////////////////////////////////////////
-
-    public static void drawVerticalHallwayDown(int x, int y, int len){
-        for (int i = y; i > (y - len); i -= 1){
-
-            if (i == 0) {
-                break;
-            }
-            else {
-
-                MapGenerator.TETile_world[i][x + 1] = Tileset.WALL;
-                MapGenerator.TETile_world[i][x - 1] = Tileset.WALL;
-                MapGenerator.TETile_world[i][x] = Tileset.WALL;
-                blocks_to_delete[remove_size] = new Integer[]{i, x};
-                remove_size += 1;
-            }
-
-        }
-    }
-
-    public static Integer[] lastXYpos(){
-        return blocks_to_delete[remove_size];
-    }
-
-    ///////////////////////////////////////////////////////////////////////////
-
-    public static void clearTheHalls(){
-        blocks_to_delete[0] = null;
-        blocks_to_delete[remove_size-1] = null;
-        remove_size -= 2;
-
-        for (int i = 1; i < (remove_size +1); i += 1){
-            int x_cor_to_rep = blocks_to_delete[i][0];
-            int y_cor_to_rep = blocks_to_delete[i][1];
-
-            MapGenerator.TETile_world[x_cor_to_rep][y_cor_to_rep] = Tileset.NOTHING;
-        }
-
-        System.out.print(Hallway.remove_size);
-    }
-
 }
