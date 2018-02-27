5c4a18e47b5bcb379a37eff49d08154f96fb7958
Alexander Osmonov <Alexander@Alexanders-MacBook.local>
2018-02-27 01:21:22 -0800

    Revamped Alg. Working on Hallway

--- proj2/byog/Core/Main.java
+++ proj2/byog/Core/Main.java
@@ -20,7 +20,7 @@ public class Main {
         } else {
             Game game = new Game();
 //            game.playWithKeyboard();
-            game.playWithInputString("N1208301283S");
+            game.playWithInputString("N86383S");
         }
     }
 }
